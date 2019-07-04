862c8b81461813e0522140f0a0a4558f2a92bba1
Ananth <agirijak@in.ibm.com>
2019-07-04 21:12:20 +0530

    Code changes

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -58,34 +58,44 @@ class ViewController: UIViewController {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .center
+        stackView.spacing = 0
         stackView.distribution = .fillProportionally
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
+    
+    // Buttons
     lazy var hitMeButton: UIButton = {
-        let button = UIButton()
-        button.titleLabel?.text = "Hit Me"
-        button.titleLabel?.textColor = .white
-        button.setImage(#imageLiteral(resourceName: "Button-Normal"), for: .normal)
-        button.setImage(#imageLiteral(resourceName: "Button-Highlighted"), for: .highlighted)
+        let button = UIButton(type: .custom)
+        button.setTitle("Hit Me", for: .normal)
+        button.setTitleColor(UIColor(hexFromString: "#601E00"), for: .normal)
+        button.setBackgroundImage(#imageLiteral(resourceName: "Button-Normal"), for: .normal)
+        button.setBackgroundImage(#imageLiteral(resourceName: "Button-Highlighted"), for: .highlighted)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont(name: "ArialRoundedMTBold", size: 20)
+
         return button
     }()
     
     lazy var reloadButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .custom)
         button.setImage(#imageLiteral(resourceName: "StartOverIcon"), for: .normal)
         button.setBackgroundImage(#imageLiteral(resourceName: "SmallButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     lazy var infoButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .custom)
         button.setImage(#imageLiteral(resourceName: "InfoButton"), for: .normal)
         button.setBackgroundImage(#imageLiteral(resourceName: "SmallButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    
+    // Labels
     lazy var topDescLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -107,7 +117,7 @@ class ViewController: UIViewController {
     lazy var rightLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "50"
+        label.text = "100"
         label.textAlignment = .center
         label.textColor = .white
         label.font = UIFont(name: "ArialRoundedMTBold", size: 18)
@@ -117,13 +127,17 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "50"
+        label.textAlignment = .left
+        label.font = UIFont(name: "ArialRoundedMTBold", size: 20)
+        label.textColor = .white
         return label
     }()
+    
     
     lazy var scoreLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "0"
+        label.text = "Score :"
         label.textAlignment = .left
         label.font = UIFont(name: "ArialRoundedMTBold", size: 20)
         label.textColor = .white
@@ -133,15 +147,47 @@ class ViewController: UIViewController {
     lazy var roundLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "1"
+        label.text = "Round :"
+        label.textAlignment = .left
+        label.font = UIFont(name: "ArialRoundedMTBold", size: 20)
+        label.textColor = .white
         return label
     }()
+    
+    lazy var scoreValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0"
+        label.textAlignment = .left
+        label.font = UIFont(name: "ArialRoundedMTBold", size: 20)
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var roundValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "1"
+        label.textAlignment = .left
+        label.font = UIFont(name: "ArialRoundedMTBold", size: 20)
+        label.textColor = .white
+        return label
+    }()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
+        setUpValues()
     }
+    
+    private func setUpValues() {
+        scoreValueLabel.text = "0"
+        roundValueLabel.text = "1"
+        targetLabel.text = String((1...100).randomElement() ?? 0)
+    }
+        
     
     private func setUpView() {
        
@@ -155,32 +201,142 @@ class ViewController: UIViewController {
         topDescLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         topDescLabel.centerHorizontally(-40)
         
-         self.view.addSubview(scoreLabel)
-        scoreLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        scoreLabel.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        scoreLabel.leadingAnchor.constraint(equalTo: topDescLabel.trailingAnchor, constant: 5).isActive = true
-        scoreLabel.centerYAnchor.constraint(equalTo: topDescLabel.centerYAnchor).isActive = true
+         self.view.addSubview(targetLabel)
+        targetLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        targetLabel.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        targetLabel.leadingAnchor.constraint(equalTo: topDescLabel.trailingAnchor, constant: 5).isActive = true
+        targetLabel.centerYAnchor.constraint(equalTo: topDescLabel.centerYAnchor).isActive = true
+        
+        self.view.addSubview(hitMeButton)
+        hitMeButton.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        hitMeButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        hitMeButton.centerVertically()
+        hitMeButton.centerHorizontally()
+        
         
         sliderStackView.addArrangedSubview(leftLabel)
         sliderStackView.addArrangedSubview(slider)
         sliderStackView.addArrangedSubview(rightLabel)
         self.view.addSubview(sliderStackView)
         
-        sliderStackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 80).isActive = true
+        sliderStackView.bottomAnchor.constraint(equalTo: self.hitMeButton.topAnchor, constant: -30).isActive = true
         sliderStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.7).isActive = true
         sliderStackView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         sliderStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
 
         slider.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        slider.widthAnchor.constraint(equalTo: sliderStackView.widthAnchor, multiplier: 0.80).isActive = true
+        slider.widthAnchor.constraint(equalTo: sliderStackView.widthAnchor, multiplier: 0.93).isActive = true
 
         leftLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         rightLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        leftLabel.widthAnchor.constraint(equalTo: sliderStackView.widthAnchor, multiplier: 0.1).isActive = true
+        rightLabel.widthAnchor.constraint(equalTo: sliderStackView.widthAnchor, multiplier: 0.055).isActive = true
+        
+        
+       
+        
+        self.view.addSubview(roundLabel)
+        roundLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        roundLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        roundLabel.leadingAnchor.constraint(equalTo: hitMeButton.trailingAnchor, constant: 40).isActive = true
+        roundLabel.topAnchor.constraint(equalTo: hitMeButton.bottomAnchor, constant: 30).isActive = true
+        
+        self.view.addSubview(roundValueLabel)
+        
+        roundValueLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        roundValueLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        roundValueLabel.leadingAnchor.constraint(equalTo: roundLabel.trailingAnchor).isActive = true
+        roundValueLabel.centerYAnchor.constraint(equalTo: roundLabel.centerYAnchor).isActive = true
+        
+        
+        self.view.addSubview(scoreValueLabel)
+        scoreValueLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        scoreValueLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        scoreValueLabel.trailingAnchor.constraint(equalTo: hitMeButton.leadingAnchor).isActive = true
+        scoreValueLabel.topAnchor.constraint(equalTo: hitMeButton.bottomAnchor, constant: 30).isActive = true
+        
+        self.view.addSubview(scoreLabel)
+//
+        scoreLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        scoreLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        scoreLabel.trailingAnchor.constraint(equalTo: scoreValueLabel.leadingAnchor).isActive = true
+        scoreLabel.centerYAnchor.constraint(equalTo: scoreValueLabel.centerYAnchor).isActive = true
+
+
+        self.view.addSubview(infoButton)
+        infoButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        infoButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        infoButton.leadingAnchor.constraint(equalTo: roundValueLabel.trailingAnchor, constant: 20).isActive = true
+        infoButton.centerYAnchor.constraint(equalTo: roundValueLabel.centerYAnchor).isActive = true
+        
+        
+        self.view.addSubview(reloadButton)
+        reloadButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        reloadButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        reloadButton.trailingAnchor.constraint(equalTo: scoreLabel.leadingAnchor, constant: -60).isActive = true
+        reloadButton.centerYAnchor.constraint(equalTo: scoreLabel.centerYAnchor).isActive = true
+
     }
 
 
 }
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    convenience init(hexFromString:String, alpha:CGFloat = 1.0) {
+        var cString:String = hexFromString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        var rgbValue:UInt32 = 10066329 //color #999999 if string has wrong format
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) == 6) {
+            Scanner(string: cString).scanHexInt32(&rgbValue)
+        }
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+    
+    func lighter(by percentage: CGFloat = 10.0) -> UIColor {
+        return self.adjust(by: abs(percentage))
+    }
+    
+    func darker(by percentage: CGFloat = 10.0) -> UIColor {
+        return self.adjust(by: -abs(percentage))
+    }
+    
+    func adjust(by percentage: CGFloat) -> UIColor {
+        var alpha, hue, saturation, brightness, red, green, blue, white : CGFloat
+        (alpha, hue, saturation, brightness, red, green, blue, white) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        
+        let multiplier = percentage / 100.0
+        
+        if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+            let newBrightness: CGFloat = max(min(brightness + multiplier*brightness, 1.0), 0.0)
+            return UIColor(hue: hue, saturation: saturation, brightness: newBrightness, alpha: alpha)
+        }
+        else if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            let newRed: CGFloat = min(max(red + multiplier*red, 0.0), 1.0)
+            let newGreen: CGFloat = min(max(green + multiplier*green, 0.0), 1.0)
+            let newBlue: CGFloat = min(max(blue + multiplier*blue, 0.0), 1.0)
+            return UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: alpha)
+        }
+        else if self.getWhite(&white, alpha: &alpha) {
+            let newWhite: CGFloat = (white + multiplier*white)
+            return UIColor(white: newWhite, alpha: alpha)
+        }
+        
+        return self
+    }
+}
+
 
 extension UIView {
     
